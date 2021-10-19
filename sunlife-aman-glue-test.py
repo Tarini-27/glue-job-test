fd2eafa925939cc264c341f8cf28a77db3e3a510
Tarini-27 <54857008+Tarini-27@users.noreply.github.com>
2021-10-19 20:20:27 +0530

    Update sunlife-aman-glue-test.py

--- sunlife-aman-glue-test.py
+++ sunlife-aman-glue-test.py
@@ -15,6 +15,7 @@ from pyspark.sql import SQLContext
 # glueContext = GlueContext(SparkContext.getOrCreate())
 
 
+
 # datasource0 = glueContext.create_dynamic_frame.from_catalog(database = 'dbcrawler', table_name = 'sunlife-cyber-sec-testtestset_json')
 # datasource0.printSchema()
 ## @type: ApplyMapping
