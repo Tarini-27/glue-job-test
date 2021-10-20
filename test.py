41b88bc7938a3d0172207896ada259a900a6e90b
Tarini-27 <54857008+Tarini-27@users.noreply.github.com>
2021-10-20 19:13:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -69,7 +69,7 @@ def handler():
                 res = res.decode("utf-8")
                 print(res)
                 #print(res.returncode)
-                if res.returncode==1:
+                if res==1:
                     print("Test Passed")
                 else:
                     print("Test Failed")
