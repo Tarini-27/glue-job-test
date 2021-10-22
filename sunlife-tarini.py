a631a737e9ee6944d8c6ef0b62ff3d6d546c3ba4
Tarini-27 <54857008+Tarini-27@users.noreply.github.com>
2021-10-22 09:48:41 +0530

    testing

--- sunlife-tarini.py
+++ sunlife-tarini.py
@@ -41,6 +41,6 @@ from pyspark.sql.functions import col
 def filter_spark_data_frame(
     dataframe,
     column_name='age',
-    value=10,
+    value=20,
 ):
     return dataframe.where(col(column_name) > value)
